3059a5fcf766503551005ba48ddc22efafe76b7b
Andrey Nazarenko <skiminok86@gmail.com>
2023-03-09 12:35:40 -0500

    fix child error + optimaze span

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -8,24 +8,27 @@ const Users = () => {
         setUsers((prevState) => prevState.filter((user) => user._id !== userId))
     }
     const renderPhrase = (number) => {
-        return number <= 4 && number > 1
-            ? number + " человека тусанет с тобой сегодня"
-            : number + " человек тусанет с тобой сегодня"
-    }
-
-    if (users.length === 0) {
+        let classes = "badge fs-4 bg-"
+        number === 0 ? classes += "danger" : classes += "primary"
+        
         return (
-            <span className="badge bg-danger fs-4">
-                Никто с тобой не тусанет
+            <span className={classes}>
+                {number === 0
+                    ? "Никто с тобой не тусанет"
+                    : number <= 4 && number > 1
+                    ? number + " человека тусанут с тобой сегодня"
+                    : number + " человек тусанет с тобой сегодня"}
             </span>
         )
     }
 
+    if (users.length === 0) {
+        return renderPhrase(users.length)
+    }
+
     return (
         <>
-            <span className="badge bg-primary fs-4">
-                {renderPhrase(users.length)}
-            </span>
+            {renderPhrase(users.length)}
             <table className="table">
                 <thead>
                     <tr>
@@ -39,37 +42,18 @@ const Users = () => {
                 </thead>
                 <tbody className="table-group-divider">
                     {users.map((user) => (
-                        //с этого места вылезает ошибка, не могу отловить
-                        //Warning: Each child in a list should have a unique "key" prop.
-                        //вроде бы везде прописаны уникальные key
-                        //даже если оставить map пустым, все равно выдает ошибку, в общем нужен хелп
-                        <tr>
-                            <td key={user.name}>{user.name}</td>
+                        <tr key={user._id}>
+                            <td>{user.name}</td>
                             <td>
                                 {user.qualities.map((quality) => (
-                                    <span
-                                        className={
-                                            "badge bg-" +
-                                            quality.color +
-                                            " me-2"
-                                        }
-                                    >
-                                        {quality.name}
-                                    </span>
+                                    <span key={quality._id} className={"badge bg-" + quality.color + " me-2"}>{quality.name}</span>
                                 ))}
                             </td>
-                            <td key={user.profession.name}>
-                                {user.profession.name}
-                            </td>
-                            <td key={user.completedMeetings}>
-                                {user.completedMeetings}
-                            </td>
-                            <td key={user.rate}>{user.rate + "/5"}</td>
+                            <td>{user.profession.name}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate + "/5"}</td>
                             <td>
-                                <button
-                                    className="btn btn-danger btn-sm"
-                                    onClick={() => handleDelete(user._id)}
-                                >
+                                <button className="btn btn-danger btn-sm" onClick={() => handleDelete(user._id)}>
                                     delete
                                 </button>
                             </td>
